ae0700bdfb3bceb4fb74e25a7c557d47aa78a31d
James Jason <jamesjason712@gmail.com>
2017-04-10 19:07:02 -0400

    add plan model

---
+++ db/migrate/20170410230655_create_plans.rb
@@ -0,0 +1,10 @@
+class CreatePlans < ActiveRecord::Migration
+  def change
+    create_table :plans do |t|
+      t.string :name
+      t.references :product, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
