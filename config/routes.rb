2b5e8a6d49fdec0dd5ee4cd85e95c2ef07788747
pbortnick <pbortnick@gmail.com>
2018-01-30 09:57:53 -0500

    updated webhook

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :subscription_items
 
 
-  mount StripeEvent::Engine, at: '/webhook'
+  mount StripeEvent::Engine, at: 'stripe#webhook'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
